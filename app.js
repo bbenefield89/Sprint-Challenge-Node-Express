7860e78f0156b04324d4613b66965906e95ec906
bbenefield89 <bsquared18@gmail.com>
2018-06-01 18:19:09 -0400

    feat(app.js): add new middleware to use `actionroutes` `/api/actions` feat(actionroutes.js): `GET: /` returns all actions feat(actionroutes.js): `GET: /:id` returns a single action

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const projectRoute = require('./routes/projectroutes');
+const actionRoute = require('./routes/actionroutes');
 
 app.use(express.json());
 app.use('/api/projects', projectRoute);
+app.use('/api/actions', actionRoute);
 
 app.listen(port, () => console.log(`Server listening on port: ${ port }`));
