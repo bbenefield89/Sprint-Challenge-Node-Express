74a269d2f0e192cc7cc41a452c09c947c91cba4a
bbenefield89 <bsquared18@gmail.com>
2018-06-01 18:19:24 -0400

    feat(app.js): add new middleware to use `actionroutes` `/api/actions` feat(actionroutes.js): `GET: /` returns all actions feat(actionroutes.js): `GET: /:id` returns a single action

---
+++ routes/actionroutes.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+
+const db = require('../data/helpers/actionModel');
+
+// error handler messages
+const errorMessages = {
+  defaultErr: 'Something has gone horribly wrong. Check back later',
+  fourOhFour: 'Could not find resource',
+  postBadRequest: 'Please provide a name and a description'
+};
+
+// TODO: automate the message used depending on the `statusCode` given
+// error handler
+const error = (res, statusCode, message) => {
+  res.status(statusCode).json({ error: message });
+}
+
+/*************************
+** CUSTOM MIDDLEWARE **
+*************************/
+// db.get()
+const dbGet = (req, res) => {
+  const { id } = req.params;
+  const { defaultErr, fourOhFour } = errorMessages;
+  db.get(id)
+    .then(data => {
+      return (data.length === 0) ? error(res, 404, fourOhFour) : res.json(data);
+    })
+    .catch(data => error(res, 500, defaultErr));
+};
+
+/*************************
+** ROUTE / **
+*************************/
+// get
+router.get('/', dbGet);
+
+/*************************
+** ROUTE /:id **
+*************************/
+// get
+router.get('/:id', dbGet);
+
+module.exports = router;
